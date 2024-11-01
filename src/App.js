038ec80872c6848c738718b1a3c3b2985ad532e5
malo-dev <leadermushio377@gmail.com>
2024-11-01 08:31:14 +0200

    Changes App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state,setState] = useState({
+    stateArray : ['None'],
+    isFilledString : '',
+    incrementedValue : 0
+  })
+  let arrayOfTasks = ['yufytct']
+  let arrayTasks = [''] ;
+  
+  const handleChange = (e) =>{
+    let valueGet = e.target.value
+    setState({
+      stateArray : [...arrayOfTasks],
+    isFilledString : valueGet,
+    })
+  }
+  const handleSubmit = () =>{
+      arrayOfTasks.push(state.isFilledString)
+      setState({
+        stateArray : [...arrayOfTasks],
+        incrementedValue : state.incrementedValue +1
+      })
+      arrayTasks[state.incrementedValue] = arrayOfTasks[arrayOfTasks.length-1]
+      
+      console.log( arrayTasks)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div>
+      <input placeholder='entrer la tache' onChange={handleChange}/>
+      <button onClick={handleSubmit}>Ajouter</button>
+      <button>Suprimer</button>
+      <button>Modifier</button>
+      </div>
+
+      <ul>
+
+    {
+      state.stateArray.map((task,index)=>{
+          return (
+            <li key={index}>
+            {task} {index}
+          </li>
+          )
+      })
+    }
+      </ul>
     </div>
   );
 }
